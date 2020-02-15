1a54a86b252c4f9c628bdc7bac375bccbdc654fb
Shubhank Singhal <45504946+ShubhankSinghal@users.noreply.github.com>
2020-02-15 19:23:02 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 var t=null;
 var displayImgCount = 0;
 
-window.open = function() {};
-        window.print = function() {};
-        // Support hover state for mobile.
-        if (false) {
-            window.ontouchstart = function() {};
-        }
+var i = 0;
+var txt = 'HAPPY BIRTHDAY';
+var txt1= "PAKHHHHHHH"
+var speed = 200;
 
-if (document.location.search.match(/type=embed/gi)) {
-    window.parent.postMessage('resize', "*");
+window.onload = function(){
+    cycleImage();
+    typeWriter();
 }
+
+function cycleImage(){
+    if (displayImgCount !== 0) {
+        document.getElementById(displayImgCount).style.display = "none";
+    }
+    displayImgCount = displayImgCount === 2 ? 1 : displayImgCount + 1;
+    document.getElementById(displayImgCount).style.display = "block";
+    if(t){
+        clearTimeout(t);
+        t = null;
+    }
+    t=setTimeout(cycleImage, 1000);
+}
+
+
+
+function typeWriter() {
+    if (i < txt.length) {
+        document.getElementById("demo1").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+}
+
+
